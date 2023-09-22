65b83e37460d41555e4da2bf2a5d0ac0f9edcac7
Cauabc <cauacardosoyt@gmail.com>
2023-09-22 14:45:44 -0300

    Some paths fixed

--- js/script.js
+++ js/script.js
@@ -21,23 +21,23 @@ let confirmaBotao = document.querySelector(".confirma")
 let infoImage = document.querySelector(".info-image")
 let textName = document.querySelector(".nome")
 let partido = document.querySelector(".partido")
-var audio = new Audio('../audio/confirma-urna.mp3');
+var audio = new Audio('audio/confirma-urna.mp3');
 
 confirmaBotao.addEventListener('click', function() {
   if (inputArea1.value && inputArea2.value){
     audio.play();
 
     if (inputArea1.value == "2" && inputArea2.value == "2"){
-      infoImage.style.backgroundImage = "url('../img/bolsonara.jpeg')";
+      infoImage.style.backgroundImage = "url('img/bolsonara.jpeg')";
       textName.innerText = "Jaira Messias Bolsonara"
       partido.innerText = "PL"
     } else if (inputArea1.value == "1" && inputArea2.value == "3"){
-      infoImage.style.backgroundImage = "url('../img/felipe.jpeg')";
+      infoImage.style.backgroundImage = "url('img/felipe.jpeg')";
       textName.innerText = "Felipao 'Gamer' Galati"
       partido.innerText = "PT"
 
     }else if (inputArea1.value == "1" && inputArea2.value == "7"){
-      infoImage.style.backgroundImage = "url('../img/paulo.jpg')";
+      infoImage.style.backgroundImage = "url('img/paulo.jpg')";
       textName.innerText = "Paulo Jesus Kogos"
       partido.innerText = "PCdoB"
     }
